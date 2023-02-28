a287f06fbcb756b355391db889ae11cb71ad70bc
Unknown2805 <nuraliali2805@gmail.com>
2023-02-28 19:11:12 +0700

    28/02/23

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,9 +7,9 @@
     <title>Landing Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href={{ asset('assets/css/main/app.css') }}>
-    <link rel="stylesheet" href={{ asset('assets/css/main/app-dark.css') }}>
-    <link rel="stylesheet" href={{ asset('assets/css/shared/iconly.css') }}>
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}">
 </head>
 
 <body style="background-color:#ecf9f7;">
@@ -79,7 +79,7 @@
                                 <div class="row">
 
                                     <div class="col-12 col-md-12">
-                                        <img src={{ asset('/storage/masjid/' . $e->image) }} class="card-img"
+                                        <img src="{{ asset('/storage/masjid/' . $e->image) }}" class="card-img"
                                             style="height:215px;" />
                                     </div>
 
